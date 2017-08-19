99025f45cd4b4b204c83bcd68b9449a7a030cb2a
dileepmama <dileepmama@gmail.com>
2017-08-19 13:52:22 -0400

    Using delete button for deleting tasks

--- app/src/main/java/com/codepath/simpletodo/TaskDatabaseUtil.java
+++ app/src/main/java/com/codepath/simpletodo/TaskDatabaseUtil.java
@@ -18,29 +18,30 @@ public class TaskDatabaseUtil {
     public static List<Task> getIncompleteTasks() {
         return SQLite.select()
                 .from(Task.class)
-                .where(Task_Table.isComplete.eq(false))
+                .where(Task_Table.name.isNotNull())
+                .and(Task_Table.isComplete.eq(false))
                 .queryList();
     }
 
     public static List<Task> getCompleteTasks() {
         return SQLite.select()
                 .from(Task.class)
-                .where(Task_Table.isComplete.eq(true))
+                .where(Task_Table.name.isNotNull())
+                .and(Task_Table.isComplete.eq(true))
                 .queryList();
     }
 
     public static List<Task> getTasksByCategory(Category category) {
         Log.i("TaskListFragment", "getTasksByCategory " + category);
         if (category.getName().equals(Category.INCOMPLETE)) {
-            Log.i("TaskListFragment", "getTasksByCategory INCOMPLETE " + category);
             return getIncompleteTasks();
         } else if (category.getName().equals(Category.COMPLETE)) {
-            Log.i("TaskListFragment", "getTasksByCategory COMPLETE " + category);
             return getCompleteTasks();
         } else {
             return SQLite.select()
                     .from(Task.class)
-                    .where(Task_Table.category.eq(category.getName()))
+                    .where(Task_Table.name.isNotNull())
+                    .and(Task_Table.category.eq(category.getName()))
                     .queryList();
         }
     }
